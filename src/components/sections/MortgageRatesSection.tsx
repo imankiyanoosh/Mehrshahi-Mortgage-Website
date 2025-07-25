5a92f6ef3cde2aecd93bf83147518183cca240c3
imankiyanoosh <58703576+imankiyanoosh@users.noreply.github.com>
2025-07-24 18:52:36 -0700

    Updated MortgageRatesSection.tsx

--- src/components/sections/MortgageRatesSection.tsx
+++ src/components/sections/MortgageRatesSection.tsx
@@ -297,7 +297,7 @@ const MortgageRatesSection: React.FC = () => {
                       fullWidth 
                       size="sm" 
                       variant="outline" 
-                      className="border-blue-700 text-blue-700 hover:bg-blue-50 hover:border-blue-800 hover:text-blue-800"
+                      className="border-white text-white hover:bg-white/10 hover:border-white hover:text-white"
                     >
                       <Phone className="w-4 h-4 mr-2" />
                       Talk to Expert
